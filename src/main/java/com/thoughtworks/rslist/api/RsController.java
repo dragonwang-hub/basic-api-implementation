f481bb98acafa6ca5b238d3aaab352bfce575ac8
dragonwang-hub <915614879@qq.com>
2020-09-18 17:43:00 +0800

    [dragon] feat: refactor all test get data from database

--- src/main/java/com/thoughtworks/rslist/api/RsController.java
+++ src/main/java/com/thoughtworks/rslist/api/RsController.java
@@ -22,9 +22,6 @@ import java.util.stream.Collectors;
 public class RsController {
 
     @Autowired
-    UserController userController;
-
-    @Autowired
     UserRepository userRepository;
 
     @Autowired
