5867e4a15b7c0890e5a20eed6a500ec93ed68ea5
dragonwang-hub <915614879@qq.com>
2020-09-20 16:17:30 +0800

    [dragon] feat: finish print log for exception

--- src/main/java/com/thoughtworks/rslist/exception/MyException.java
+++ src/main/java/com/thoughtworks/rslist/exception/MyException.java
@@ -1,13 +1,20 @@
 package com.thoughtworks.rslist.exception;
 
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
 public class MyException {
+
+    Logger logger = LoggerFactory.getLogger(MyException.class);
+
     @ExceptionHandler({MyIndexOutOfBoundsException.class, IndexException.class, InvaildRsEventExcepttion.class,InvalidUserException.class})
     public ResponseEntity handleIndexOutOfBoundsException(Exception ex) {
+        logger.error(ex.getMessage());
         CommentError commentError = new CommentError(ex.getMessage());
         return ResponseEntity.badRequest().body(commentError);
     }
